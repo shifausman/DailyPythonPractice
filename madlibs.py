b1134366eb91c4f54e3cafddaf1687a6cdebc297
shifa usman <shifausman198961@gmail.com>
2025-12-22 21:04:08 +0530

    A Simple story prediction game

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+#madlivs game
+#word game where you can create a story by filling blanks with random variables
+
+adjective1 = input("Enter an adjective (to decribe zoo): ")
+noun1 = input("Enter a noun (person, animal etc..): ")
+adjective2= input("Enter an adjective (to decribe noun): ")
+verb1 = input("Enter a verb (to decribe noun): ")
+adjective3 = input("Enter an emotion: ")
+
+print(f"Today I went to a {adjective1} zoo")
+print(f"In that exihibit I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}ing")
+print(f"I was {adjective3}")
